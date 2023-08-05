5445ea9d644627d6a99083ab0f2f89eed8a8dc7c
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2023-08-05 19:49:14 +0200

    Ajout de la conversion en .webp lorsque l'image est télécharger

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,12 +1,5 @@
 const multer = require("multer");
 
-// Types de fichiers acceptés et leurs extensions correspondantes
-const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
-};
-
 // Configuration de multer pour la gestion des fichiers
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -14,8 +7,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_"); // Renommage du fichier pour éviter les espaces
-    const extension = MIME_TYPES[file.mimetype]; // Récupération de l'extension du fichier à partir du type MIME
-    callback(null, name + Date.now() + "." + extension); // Construction du nom de fichier unique
+    callback(null, name + Date.now() + "." + ".webp"); // Construction du nom de fichier unique
   },
 });
 
